58aad9b4f44433bbb02c26943e14bafdafeafbe8
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2016-01-14 15:25:44 +0000

    DSPack model

--- app/models/ds_pack.rb
+++ app/models/ds_pack.rb
@@ -1,8 +1,10 @@
 class DsPack < ActiveRecord::Base
   belongs_to :assay
-  attr_accessible :description, :msg, :status, :title
+  #attr_accessible :description, :msg, :status, :title
 
-  validates :title,  presence: true
+  validates :title,  presence: true, length: { maximum: 140, minimum: 10 }
   validates :description,  presence: true
+  validates :assay_id, presence: true
+  validates :assay, presence: true
 
 end
