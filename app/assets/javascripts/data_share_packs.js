6b0b33beb5c11e73b323819e7d28b91954fd9110
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2016-01-14 20:27:50 +0000

    DataSharePack artifacts added. First implementation of new linked to the assay. Faked empty js added to fix travis build script

--- app/assets/javascripts/data_share_packs.js
+++ app/assets/javascripts/data_share_packs.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+//empty function to pass travis build which complained about index out of bound if it was an empty file
+function dataSharePacksEmpty() {
+
+}
